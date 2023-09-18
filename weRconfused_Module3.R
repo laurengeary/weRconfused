f699f128d01977e68af1b8194caf6e0e7eb0aeae
Lauren Geary <laurengeary@Laurens-MacBook-Pro-8.local>
2023-09-18 12:00:48 -0400

    Working through Module 3

---
+++ weRconfused_Module3.R
@@ -0,0 +1,24 @@
+#load libraries
+library(tidyverse)
+library(ape)
+library(nlme)
+library(geiger)
+library(caper)
+library(phytools)
+library(viridis)
+library(MuMIn)
+
+#load data
+anole <- read_csv("anole.dat.csv")
+anole.eco <- read_csv("anole.eco.csv")
+
+#merge anole data tibble with anole.eco tibble
+anole2 <- anole%>%
+  left_join(anole.eco)%>%
+  filter(!Ecomorph%in%c("U","CH"))%>%
+  na.omit()%>%
+  print()
+
+anole.log <- anole2%>%
+  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
+
